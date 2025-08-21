1dd5c2f41d40f799cb9b8898b594ec95e1693f1c
Fahim MIS <fahim.wakrah@gmail.com>
2025-08-21 17:36:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    
+      <h1 className='text-8xl text-center'> React MIS Router Data</h1>
+   
+      <p className="read-the-docs border-2 border-amber-400">
+        Ultimate MIS Dashboard
+      </p>
+    </>
+  )
+}
+
+export default App
