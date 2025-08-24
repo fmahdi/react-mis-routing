2bb211fcb65bbff481003a437db961c5fd9a1c56
Fahim MIS <fahim.wakrah@gmail.com>
2025-08-24 14:19:33 +0600

    created a basic navbar and added to the projects

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Header = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h1 className='text-8xl text-center'> React MIS Router Data</h1>
         </div>
     );
