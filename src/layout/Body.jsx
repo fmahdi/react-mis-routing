20faadd6477b248c6358a170837719648df02dac
Fahim MIS <fahim.wakrah@gmail.com>
2025-08-24 16:18:46 +0600

    created a formula to have constant body height for the website

--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Body = () => {
     return (
-        <div>
+        <div className="min-h-[calc(100vh-165px)]">
             <p className="read-the-docs border-2 border-amber-400">
                 Ultimate MIS Dashboard
             </p>
